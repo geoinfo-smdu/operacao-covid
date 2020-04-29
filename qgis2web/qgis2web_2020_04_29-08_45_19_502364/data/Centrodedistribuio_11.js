68fb3692fc69e3c5d7e86020b9eab4dc9f6edc56
Fernando Gomes <feromes@gmail.com>
2020-04-29 08:54:58 -0300

    Adicionando UBSs por extrema pobreza

---
+++ qgis2web/qgis2web_2020_04_29-08_45_19_502364/data/Centrodedistribuio_11.js
@@ -0,0 +1,8 @@
+var json_Centrodedistribuio_11 = {
+"type": "FeatureCollection",
+"name": "Centrodedistribuio_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": 9.0, "eq_tipo": "SALAS DE SHOWS E CONCERTOS", "eq_nome": "CRUZ VERMELHA - ANFITEATRO", "eq_enderec": "AV MOREIRA GUIMARÃES,  699", "eq_telefon": "6917-3710", "eq_bairro": "HIGIENÓPOLIS", "eq_cep": null, "eq_distr": "SAUDE", "eq_subpref": "VILA MARIANA", "eq_regiao5": "SUL", "FUNCAO": "CENTRO DE DISTRIBUIÇÃO E FRACIONAMENTO DE MATERIAL DE LIMPEZA E HIGIENE" }, "geometry": { "type": "Point", "coordinates": [ -46.656283805113524, -23.613349674624914 ] } }
+]
+}
